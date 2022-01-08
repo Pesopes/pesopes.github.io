f91c3c804cb9d4fe0d4f8083f0af30389c0fa913
Pesopes <88830512+Pesopes@users.noreply.github.com>
2022-01-08 13:24:48 +0100

    Ascii even better

--- Sejdr/js/canvas.js
+++ Sejdr/js/canvas.js
@@ -1,6 +1,37 @@
+// - add toggleable greyscale (just apply the function or not in draw)
+// - more settings
+// - more shader functions (smoothstep, colormix, idk, blah, ...)
+//
 var canvasHeight = 800;
 var canvasWidth = 800;
-var resScale = 3; // higher = more pixelated 
+var resScale = 3; // higher = more pixelated , changes trough slider
+// ONLOAD
+function load() {
+    console.log('%c My bad shader', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    // configure slider : resolution
+    var resSlider = document.getElementById("resSlider");
+    resSlider.oninput = function () {
+        //why cant I use this.value i do not know, I know you haave to specify that its a slider (or input in general)
+        resScale = parseFloat(resSlider.value);
+        var resNum = document.getElementById("resNum");
+        resNum.innerHTML = "  " + resSlider.value;
+        draw();
+    };
+    var codeInput = document.getElementById("code");
+    codeInput.innerHTML = shaderMain.toString();
+    //draw()
+    var base_image = new Image();
+    base_image.crossOrigin = "anonymous";
+    base_image.src = "js/img/hr.png";
+    base_image.onload = function () {
+        ctx.drawImage(base_image, 0, 0, 800, 800);
+    };
+}
+// DRAW FUNCTIONS
 function draw() {
     clearCanvas();
     var canvas = document.getElementById("canvas");
@@ -13,29 +44,13 @@ function draw() {
             for (var y = 0; y < screenW; y++) {
                 //shader colour
                 var sc = shaderMain(x, y, screenW, screenH, timeAtStart);
-                ctx.fillStyle = canvasRgba([sc[0] * 0.3 + sc[1] * 0.3 + sc[2] * 0.3, sc[0] * 0.3 + sc[1] * 0.3 + sc[2] * 0.3, sc[0] * 0.3 + sc[1] * 0.3 + sc[2] * 0.3]);
+                ctx.fillStyle = canvasRgba(sc);
                 ctx.fillRect(x * resScale, y * resScale, resScale, resScale);
             }
         }
     }
 }
-function shaderMain(x, y, w, h, time) {
-    var fragColor = [0, 0, 0];
-    var uv = [x / w, y / h];
-    fragColor[0] = distance(uv, [0.5, 0.5]) * Math.sin(time);
-    fragColor[1] = distance(uv, [0.8, 0.5]);
-    fragColor[2] = distance(uv, [0.8, 0.9]);
-    return fragColor;
-}
-function step(xy, edge) {
-    if (xy[0] > edge && xy[1] > edge)
-        return 1;
-    else
-        return 0;
-}
-function distance(xy1, xy2) {
-    return Math.sqrt((xy1[0] - xy2[0]) * (xy1[0] - xy2[0]) + (xy1[1] - xy2[1]) * (xy1[1] - xy2[1]));
-}
+//the asciifromcanvas func is better
 function asciiDraw() {
     var asciiSymbols = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
     var screenW = 50;
@@ -55,6 +70,94 @@ function asciiDraw() {
         return asciiSymbols[Math.round((asciiSymbols.length - 1) * colour)];
     }
 }
+function asciiFromCanvas() {
+    var asciiSymbols = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+    var drawing = "";
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var screenW = canvas.width;
+    var screenH = canvas.height;
+    var blockSize = 20;
+    for (var y = 0; y < screenH / blockSize; y++) {
+        for (var x = 0; x < screenW / blockSize; x++) {
+            //ctx.getImageData(x,y,blockSize,blockSize).data
+            drawing += convertColorToSymbol(averageColour(ctx.getImageData(x * blockSize, y * blockSize, blockSize, blockSize).data)) + " ";
+        }
+        drawing += "\n";
+    }
+    return drawing;
+    function convertColorToSymbol(colour) {
+        return asciiSymbols[Math.round((asciiSymbols.length - 1) * colour)];
+    }
+    function averageColour(data) {
+        var averages = [];
+        for (var i = 0; i < data.length; i += 4) {
+            var avg = (data[i] + data[i + 1] + data[i + 2] + data[i + 3]) / 4;
+            averages.push(avg);
+        }
+        var finalAverage = 0;
+        for (var index = 0; index < averages.length; index++) {
+            finalAverage += averages[index];
+        }
+        return Math.round(finalAverage / averages.length) / 255;
+    }
+}
+//
+// SHADER
+//
+function shaderMain(x, y, w, h, time) {
+    var fragColor = [0, 0, 0];
+    var uv = [x / w, y / h];
+    fragColor[1] = stepVec1(distance(uv, [0.5, 0.5]), 0.5);
+    fragColor[2] = stepVec1(distance(uv, [0.5, 0.5]), 0.5);
+    return fragColor;
+}
+function runCustomShader() {
+    var headID = document.getElementsByTagName("head")[0];
+    // let newScript = <HTMLScriptElement>document.getElementById("customShader");
+    // if (newScript === null) {
+    //     newScript = document.createElement("script")
+    // }
+    var newScript = document.createElement("script");
+    newScript.id = "customShader";
+    newScript.type = "text/javascript";
+    newScript.innerHTML = document.getElementById("code").value;
+    headID.appendChild(newScript);
+}
+//
+// SHADER FUNCTIONS
+//
+function stepVec2(xy, edge) {
+    if (xy[0] > edge && xy[1] > edge)
+        return 1;
+    else
+        return 0;
+}
+function stepVec1(x, edge) {
+    if (x > edge)
+        return 1;
+    else
+        return 0;
+}
+function distance(xy1, xy2) {
+    return Math.sqrt((xy1[0] - xy2[0]) * (xy1[0] - xy2[0]) + (xy1[1] - xy2[1]) * (xy1[1] - xy2[1]));
+}
+//
+// COLOUR CONVERTING
+//
+//converts to the weird rgba format for css/canvas/idkwhat styling, uses 0.0-1.0 like in shaders
+function canvasRgba(rgb, a) {
+    if (a === void 0) { a = 1.0; }
+    return "rgba(" + rgb[0] * 255 + "," + rgb[1] * 255 + "," + rgb[2] * 255 + "," + a + ")";
+}
+function rgb2grey(rgb, a) {
+    if (a === void 0) { a = 1.0; }
+    var greyScale = rgb[0] * 0.299 + rgb[1] * 0.587 + rgb[2] * 0.144;
+    return [greyScale, greyScale, greyScale];
+}
+//
+// CANVAS FUNCTIONS
+//
 // just copied somehitng not working really TODO
 function saveCanvasImg() {
     var canvas = document.getElementById("canvas");
@@ -68,24 +171,4 @@ function clearCanvas() {
         var ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
-}
-//converts to the weird rgba format for css styling, uses 0.0-1.0 like in shaders
-function canvasRgba(rgb, a) {
-    if (a === void 0) { a = 1.0; }
-    return "rgba(" + rgb[0] * 255 + "," + rgb[1] * 255 + "," + rgb[2] * 255 + "," + a + ")";
-}
-function load() {
-    var canvas = document.getElementById("canvas");
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
-    var resSlider = document.getElementById("resSlider");
-    resSlider.oninput = function () {
-        //why cant I use this.value i do not know, I know you haave to specify that its a slider (or input in general)
-        resScale = parseFloat(resSlider.value);
-        resSlider.innerHTML = resSlider.value;
-        draw();
-    };
-    draw();
-    setInterval(draw, 10000);
-    console.log(asciiDraw());
 }
--- Sejdr/js/canvas.ts
+++ Sejdr/js/canvas.ts
@@ -1,7 +1,45 @@
+// - add toggleable greyscale (just apply the function or not in draw)
+// - more settings
+// - more shader functions (smoothstep, colormix, idk, blah, ...)
+//
+
 let canvasHeight = 800;
 let canvasWidth = 800;
-let resScale = 3; // higher = more pixelated 
+let resScale = 3; // higher = more pixelated , changes trough slider
 
+// ONLOAD
+function load(){
+
+    console.log('%c My bad shader', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+
+    let canvas = <HTMLCanvasElement> document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    // configure slider : resolution
+    let resSlider = <HTMLInputElement> document.getElementById("resSlider");
+    resSlider.oninput = function(){
+        //why cant I use this.value i do not know, I know you haave to specify that its a slider (or input in general)
+        resScale = parseFloat(resSlider.value);
+        let resNum = <HTMLInputElement> document.getElementById("resNum");
+        resNum.innerHTML = "  " + resSlider.value;
+        draw()
+    }
+
+    var codeInput = document.getElementById("code");
+    codeInput.innerHTML = shaderMain.toString()
+
+    //draw()
+    let base_image = new Image();
+    base_image.crossOrigin = "anonymous";
+    base_image.src = "js/img/hr.png";
+    base_image.onload = function(){
+        ctx.drawImage(base_image, 0, 0,800,800);
+    }
+}
+
+// DRAW FUNCTIONS
 function draw(){
     clearCanvas()
     let canvas = <HTMLCanvasElement>document.getElementById("canvas");
@@ -10,39 +48,18 @@ function draw(){
         const screenW = canvas.width/resScale;
         const screenH = canvas.height/resScale
         const timeAtStart = Date.now()
-
         for (let x = 0; x < screenH; x++) {
             for (let y = 0; y < screenW; y++) {
                 //shader colour
                 let sc = shaderMain(x,y,screenW,screenH,timeAtStart);
-                ctx.fillStyle = canvasRgba([sc[0]*0.3+sc[1]*0.3+sc[2]*0.3,sc[0]*0.3+sc[1]*0.3+sc[2]*0.3,sc[0]*0.3+sc[1]*0.3+sc[2]*0.3]);
+                ctx.fillStyle = canvasRgba(sc);
                 ctx.fillRect(x*resScale,y*resScale,resScale,resScale);
             }
         }
     }
 }
 
-function shaderMain(x:number,y:number,w:number,h:number,time:number):[number,number,number]{
-    let fragColor: [number,number,number] = [0,0,0];
-    let uv: [number,number] = [x/w,y/h]
-    fragColor[0] = distance(uv,[0.5,0.5]) * Math.sin(time)
-    fragColor[1] = distance(uv,[0.8,0.5])
-    fragColor[2] = distance(uv,[0.8,0.9])
-    return fragColor;
-}
-
-
-function step(xy:[number,number],edge:number){
-
-    if(xy[0] > edge && xy[1]>edge)
-        return 1
-    else return 0
-}
-
-function distance(xy1:[number,number],xy2:[number,number]){
-    return Math.sqrt((xy1[0]-xy2[0])*(xy1[0]-xy2[0])+(xy1[1]-xy2[1])*(xy1[1]-xy2[1]))
-}
-
+//the asciifromcanvas func is better
 function asciiDraw(){
     const asciiSymbols = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
     const screenW = 50;
@@ -62,6 +79,110 @@ function asciiDraw(){
         return asciiSymbols[Math.round((asciiSymbols.length-1) * colour)]
     }
 }
+
+function asciiFromCanvas(){
+    const asciiSymbols = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+    let drawing = "";
+
+    let canvas = <HTMLCanvasElement>document.getElementById("canvas");
+    let ctx : CanvasRenderingContext2D = canvas.getContext("2d");
+
+    const screenW = canvas.width;
+    const screenH = canvas.height;
+    let blockSize = 20
+
+    for (let y = 0; y < screenH/blockSize ; y++) {
+        for (let x = 0; x < screenW/blockSize ; x++) {
+            //ctx.getImageData(x,y,blockSize,blockSize).data
+            drawing += convertColorToSymbol(averageColour(ctx.getImageData(x*blockSize,y*blockSize,blockSize,blockSize).data)) + " "
+        }
+        drawing += "\n"
+    }
+    return drawing
+    function convertColorToSymbol(colour:number){
+        return asciiSymbols[Math.round((asciiSymbols.length-1) * colour)]
+    }
+
+    function averageColour(data){
+        let averages = []
+        for (let i = 0; i < data.length; i+=4) {
+            let avg = (data[i] + data[i+1] + data[i+2] + data[i+3])/4
+            averages.push(avg)
+        }
+        let finalAverage= 0;
+        for (let index = 0; index < averages.length; index++) {
+            finalAverage += averages[index]
+        }
+        return Math.round(finalAverage / averages.length) /255
+    }
+}
+
+//
+// SHADER
+//
+
+function shaderMain(x:number,y:number,w:number,h:number,time:number):[number,number,number]{
+    let fragColor: [number,number,number] = [0,0,0];
+    let uv: [number,number] = [x/w,y/h]
+    fragColor[1] = stepVec1(distance(uv,[0.5,0.5]),0.5)
+    fragColor[2] = stepVec1(distance(uv,[0.5,0.5]),0.5)
+
+    return fragColor;
+}
+
+function runCustomShader() {
+    let headID = document.getElementsByTagName("head")[0];
+    // let newScript = <HTMLScriptElement>document.getElementById("customShader");
+    // if (newScript === null) {
+    //     newScript = document.createElement("script")
+    // }
+    let newScript = document.createElement("script")
+
+    newScript.id = "customShader"
+    newScript.type = "text/javascript";
+    newScript.innerHTML = (<HTMLTextAreaElement> document.getElementById("code")).value;
+    headID.appendChild(newScript);
+}
+
+//
+// SHADER FUNCTIONS
+//
+
+function stepVec2(xy:[number,number],edge:number){
+
+    if(xy[0] > edge && xy[1]>edge)
+        return 1
+    else return 0
+}
+
+function stepVec1(x,edge:number){
+
+    if(x > edge)
+        return 1
+    else return 0
+}
+
+function distance(xy1:[number,number],xy2:[number,number]){
+    return Math.sqrt((xy1[0]-xy2[0])*(xy1[0]-xy2[0])+(xy1[1]-xy2[1])*(xy1[1]-xy2[1]))
+}
+//
+// COLOUR CONVERTING
+//
+
+//converts to the weird rgba format for css/canvas/idkwhat styling, uses 0.0-1.0 like in shaders
+function canvasRgba(rgb:[number,number,number],a=1.0){
+    return "rgba("+rgb[0]*255+","+rgb[1]*255+","+rgb[2]*255+","+a+")";
+
+}
+
+function rgb2grey(rgb:[number,number,number],a=1.0) : [number,number,number]{
+    let greyScale = rgb[0]*0.299+rgb[1]*0.587+rgb[2]*0.144
+    return [greyScale,greyScale,greyScale]
+}
+
+//
+// CANVAS FUNCTIONS
+//
 
 // just copied somehitng not working really TODO
 function saveCanvasImg():void{
@@ -83,33 +204,6 @@ function clearCanvas(){
     }
 }
 
-//converts to the weird rgba format for css styling, uses 0.0-1.0 like in shaders
-function canvasRgba(rgb:[number,number,number],a=1.0){
-
-    return "rgba("+rgb[0]*255+","+rgb[1]*255+","+rgb[2]*255+","+a+")";
-
-}
-
-function load(){
-    let canvas = <HTMLCanvasElement> document.getElementById("canvas");
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
-
-    let resSlider = <HTMLInputElement> document.getElementById("resSlider");
-    resSlider.oninput = function(){
-        //why cant I use this.value i do not know, I know you haave to specify that its a slider (or input in general)
-        resScale = parseFloat(resSlider.value);
-        resSlider.innerHTML = resSlider.value;
-        draw()
-    }
 
 
 
-
-
-
-
-    draw()
-    setInterval(draw,10000);
-    console.log(asciiDraw())
-}
